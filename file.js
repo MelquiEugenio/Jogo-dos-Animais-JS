60917e183b2b37408d0603b039138ac3abcfe4a9
Melqui Eugenio <melqui.eug@gmail.com>
2017-06-15 19:10:29 -0300

    Signed-off-by: Melqui Eugenio <melqui.eug@gmail.com>

--- file.js
+++ file.js
@@ -1,17 +1,89 @@
-var answer = {
-  y: function () {
-    document.getElementById("msg").innerHTML = "You've clicked Yes.";
-  },
-  n: function () {
-  document.getElementById("msg").innerHTML = "You've clicked No.";
-  }
+//----------------Classificação
+
+class Classificacao {
+
+	var ladoNao, ladoSim, caracteristica;
+
+	constructor(caracteristica, ladoSim, ladoNao){
+		this.caracteristica = caracteristica;
+		this.ladoSim = ladoSim;
+		this.ladoNao = ladoNao;
+	}
+
+	aprendeCom(jogador) {
+		if (jogador.responda("O animal que vc pensou " + caracteristica + "?"))
+			ladoSim = ladoSim.aprendeCom(jogador);
+		else
+			ladoNao = ladoNao.aprendeCom(jogador);
+
+		return this;
+	}
 }
 
-/*
-class Node {
-  constructor(data, left, right) {
-    this.data = data;
-    this.left = left;
-    this.right right;
-  }
-}*/
+
+//----------------Animal
+
+class Animal {
+
+	var nome;
+
+	constructor(nome){
+		this.nome = nome;
+	}
+
+	aprendeCom(jogador) {
+
+		if(jogador.responda("O animal que vc pensou é o(a) " + nome + "?")) {
+			jogador.considere("Acertei de novo!");
+			return this;
+		} else {
+			var novoNome = jogador.insira("Em qual animal pensastes?");
+			var caracteristica = jogador.insira("O(a) " + novoNome + " ______," + " mas o " + nome + " não.");
+			var novoAnimal = new Animal(novoNome);
+			return new Classificacao(caracteristica, novoAnimal, this);
+		}
+
+	}
+
+}
+
+
+//----------------Jogador
+
+class Jogador {
+
+	considere(mensagem){
+    alert(mensagem);
+	}
+
+	responda(pergunta) {
+    document.getElementById("msg").innerHTML = pergunta;
+    document.getElementById("yButton").onclick = function(){
+        return true};
+    document.getElementById("nButton").onclick = function(){
+        return false};
+	}
+
+	insira(pergunta){
+		var result = prompt(pergunta);
+		if (result == null) insira(pergunta);
+		return result;
+	}
+}
+
+
+//----------------Main
+
+class Mainy {
+
+	main() {
+
+		var jogador = new Jogador();
+		var raiz = new Animal("Lion");
+
+		while(true) {
+			jogador.considere("Pense em um animal.");
+			raiz = raiz.aprendeCom(jogador);
+		}
+	}
+}
